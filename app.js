87bb4d830914d14d6ab4b600a70f24dd5fcdcb6f
unknown <acostamarialauraa@gmail.com>
2025-08-08 14:57:14 -0300

    Se realizó la database

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+
+
+const userRoutes = require('./src/routes/user.routes');
+app.use('/users', userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
